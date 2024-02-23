5f81943061ed91ed10ed9c8dc1f4014380d01b23
Matteo Piccini <gnappo.1@live.it>
2024-02-23 15:50:42 -0800

    mid-walkthrough code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
-const API = "http://localhost:3001/sushis";
+const API = "http://localhost:4000/sushis";
 
 function App() {
+  const [sushi, setSushi] = useState([]);
+  const amountToSlice = 4
+
+  const handleMoreSushi = () => {
+    setSushi(currentSushiList => [...currentSushiList.slice(amountToSlice), ...currentSushiList.slice(0, amountToSlice)])
+  }
+  useEffect(() => {
+    (() => {
+      fetch(API)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('The json server is not running!')
+        }
+        return res.json()
+      })
+      .then(setSushi)
+      .catch(err => console.log(err))
+    })()
+  }, [])
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushi={sushi} handleMoreSushi={handleMoreSushi} amountToSlice={amountToSlice}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,24 @@
-import React from "react";
+import {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushi, handleMoreSushi, amountToSlice }) {
+  // const [startSushiNum, setStartSushiNum] = useState(0);
+  // const amountToSlice = 4
+
+  // const handleMoreSushi = () => {
+  //   setStartSushiNum(currentVal => {
+  //     return startSushiNum === sushi.length - amountToSlice ? 0 : currentVal + amountToSlice
+  //   })
+  // }
+
+  // const slicedSushi = sushi.slice(startSushiNum, startSushiNum + amountToSlice)
+  const renderList = sushi.slice(0, amountToSlice).map(s => <Sushi key={s.id} {...s}/>)
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {renderList}
+      <MoreButton handleMoreSushi={handleMoreSushi}/>
     </div>
   );
 }
